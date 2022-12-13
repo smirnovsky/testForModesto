1a249c18a4ad3c5ef8477ba3a808dee143e8f355
smirnovsky <happy280190@gmail.com>
2022-12-13 21:27:03 +0300

    feat: add navigation to anchor links

--- js/index.js
+++ js/index.js
@@ -11,3 +11,19 @@ document.getElementById("btn-back").addEventListener("click", function(){
     document.querySelector('.form-button__back').classList.add('hide')
     document.querySelector('.form-button__forward').classList.remove('hide')
 });
+
+
+
+document.querySelectorAll('a[href*="#start"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      const blockID = anchor.getAttribute('href').substr(1);
+      if(document.getElementById(blockID)) {
+        e.preventDefault();
+  
+        document.getElementById(blockID).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
